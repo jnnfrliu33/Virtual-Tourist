96e3d0f83be9e0663cbed22940d016adae378dc2
Jennifer Liu <jnnfrliu33@gmail.com>
2017-11-21 23:11:46 +0800

    Add saveContext to appDelegate

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -38,6 +38,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        do {
+            try CoreDataStack.sharedInstance().saveContext()
+        } catch {
+            print ("Unable to save context!")
+        }
     }
 
 
